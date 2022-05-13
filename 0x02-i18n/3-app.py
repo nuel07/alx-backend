7f0dd6fdaabef3582d98c323a5af4ea9f81c5d5b
nuel07 <yehanganep@gmail.com>
2022-05-13 08:05:24 -0700

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""basic flask app"""
+
+from flask import Flask, render_template, request, _
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+app.url_map_strict_slashes = False
+babel = Babel(app)
+
+
+class Config:
+    """ babel configuration class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ determines best match with supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=["GET"])
+def hello():
+    """ renders template """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
